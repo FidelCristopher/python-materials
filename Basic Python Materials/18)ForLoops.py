b085abaf8d16f2466bb5be38b1f44ba13117d736
Fidel Cristopher <fidelcristropher@gmail.com>
2025-04-29 05:39:55 +0700

    Add files via upload

---
+++ Basic Python Materials/18)ForLoops.py
@@ -0,0 +1,23 @@
+# for loops = execute a block of code a fixed number of times.
+            # You can iterate over a range, string, sequence, etc
+
+# for x in reversed(range(1, 11)):
+#     print(x)
+# print("HAPPY NEW YEAR")
+
+
+# for x in range(1, 11, 3):
+#     print(x)
+
+
+# credit_card = "1234-5678-9012-3456"
+
+# for x in credit_card:
+#     print(x)
+
+
+for x in range(1, 21):
+    if x == 13:
+        break
+    else:
+        print(x)
