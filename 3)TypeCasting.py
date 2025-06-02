93e17a5ff4062ab49571c420d87c0d0b409c2d2b
Fidel Cristopher <fidelcristropher@gmail.com>
2025-06-03 04:42:31 +0700

    Initial commit

---
+++ 3)TypeCasting.py
@@ -0,0 +1,21 @@
+#Typecasting = the process of converting a variable from one data type to another str(), int(), float(), bool()
+
+name = "Fidel"
+age = 19
+gpa = 3.9
+is_student = True
+
+print(type(name))
+
+gpa = int(gpa)
+print(gpa)
+
+age = float(age)
+print(age)
+
+age = str(age)
+print(age)
+print(type(age))
+
+name = bool(name)
+print(name)
---
+++ 4)UserInput.py
@@ -0,0 +1,30 @@
+#input() = A function that prompts the user to enter data
+#          Returns the entered data as a string
+
+#input("What is your name?: ")
+
+#name = input("What is your name?: ")
+#age = int(input("How old are you?: "))
+
+#age = int(age)
+#age = age + 1
+
+#print(f"Hello {name}!")
+#print("HAPPY BIRTHDAY!")
+#print(f"You are {age} years old")
+
+#EXERCISE 1
+#length = float(input("Enter the length: "))
+#width = float(input("Enter the width: "))
+#area = length * width
+
+#print(f"The area is: {area}cm^2")
+
+#EXERCISE 2
+item = input("What item would you like to buy?: ")
+price = float(input("What is the price?: "))
+quantity = int(input("How many would you like?: "))
+total = price * quantity
+
+print(f"You have bought {quantity} x {item}/s")
+print(f"Your total is: ${total}")
